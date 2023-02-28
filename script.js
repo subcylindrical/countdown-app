9e1f3118fcecc04ba632cff6758f99336365960c
subcylindrical <zacharylehouillier@gmail.com>
2023-02-28 05:53:21 -0800

    change holiday toggle list UI

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+const showHolidaysBtn = document.querySelector('.show-holidays');
+const holidayList = document.querySelector('.holiday-select-container');
+
+// Event Listeners
+showHolidaysBtn.addEventListener('click', (e) => {
+  holidayList.classList.toggle('collapse-list');
+  showHolidaysBtn.classList.toggle('float');
+});
+
+// Fetch Holiday Data
 async function fetchHolidayList() {
   const holidayData = await fetch(
     'https://date.nager.at/api/v3/NextPublicHolidays/US'
